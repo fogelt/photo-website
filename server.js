cfaff47dede123e0973ba08b6fac15cd790a660f
fogelt <167136460+fogelt@users.noreply.github.com>
2025-10-17 20:28:01 +0200

    added limiter

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ import fs from "fs";
 dotenv.config();
 const app = express();
 
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+      standardHeaders: true,
+        legacyHeaders: false,
+        });
+
+        app.use(limiter);
+        
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
